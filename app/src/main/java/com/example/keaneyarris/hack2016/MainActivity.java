a3ca76eee08ec2f6e1723f7af91efd8a71823d9e
KeaneYarris <keane.yarris@gmail.com>
2016-11-11 19:33:32 -0500

    Commit try again

---
+++ app/src/main/java/com/example/keaneyarris/hack2016/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.keaneyarris.hack2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("Commit this ");
+        System.out.println("Commit this ");
+        for (int i = 0; i < 69; i++){
+            System.out.println("Ben sucks weiner");
+        }
+
+    }
+    //aye lmao aye
+    // trying to commit still lol
+
+}
